4939f02bb418055c0a8e59126fb75f6c1f4db2f9
SPToss <sebastian.lukasz.pawlikowski@gmail.com>
2017-06-05 20:30:12 +0200

    initial Comit of NotificatorApp
    Initial comit of NotificatorApp for Android

---
+++ Activities/SettingsActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Preferences;
+using Android.Content.PM;
+
+namespace NotificatorApp.Activities
+{
+    [Activity(Label = "TEST", ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SettingsActivity : PreferenceActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            AddPreferencesFromResource(Resource.Xml.Preference);
+
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Preferences;
+using Android.Content;
+using NotificatorApp.Activities;
+using System;
+
+namespace NotificatorApp
+{
+    [Activity(Label = "NotificatorApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            Button button = FindViewById<Button>(Resource.Id.settingsButton);
+            button.Click += OnButtonClicked;
+
+        }
+
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this.ApplicationContext, typeof(SettingsActivity));
+            StartActivity(intent);
+        }
+    }
+}
+
